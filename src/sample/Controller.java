9c2d36398d9f90546b820f5c4ceb13873062a501
Mark Romphf <markromphf@gmail.com>
2020-02-04 18:01:09 -0600

    Some basic canvas rendering.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,41 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Canvas canvas;
+
+    private GraphicsContext context;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        context = canvas.getGraphicsContext2D();
+        context.setFill(Color.BLUE);
+        context.fillRect(100, 100, 250, 250);
+    }
+
     @FXML
     public void handleKeyPressed(KeyEvent keyEvent) {
-        System.out.println(String.format("%s was pressed", keyEvent.getCode()));
+        switch (keyEvent.getCode()) {
+            case G:
+                context.setFill(Color.GREEN);
+                break;
+            case R:
+                context.setFill(Color.RED);
+                break;
+            default:
+                context.setFill(Color.BLUE);
+                break;
+        }
+        context.fillRect(100, 100, 250, 250);
     }
 }
