0c70fb7456847536ee28dac750424552e6d24231
Mark Romphf <markromphf@gmail.com>
2020-02-05 14:59:08 -0600

    Render an image on the canvas.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,12 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,28 +16,25 @@ public class Controller implements Initializable {
     @FXML
     private Canvas canvas;
 
+    private Image blockImg = new Image(getClass().getResourceAsStream("brown-block.png"));
     private GraphicsContext context;
+    private double screenHeight;
+    private double screenWidth;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Rectangle2D screen = Screen.getPrimary().getBounds();
+        screenHeight = screen.getHeight();
+        screenWidth = screen.getWidth();
+        canvas.setHeight(screenHeight);
+        canvas.setWidth(screenWidth);
+
         context = canvas.getGraphicsContext2D();
-        context.setFill(Color.BLUE);
-        context.fillRect(100, 100, 250, 250);
+        context.drawImage(blockImg, 0, 0,100, 100);
     }
 
     @FXML
     public void handleKeyPressed(KeyEvent keyEvent) {
-        switch (keyEvent.getCode()) {
-            case G:
-                context.setFill(Color.GREEN);
-                break;
-            case R:
-                context.setFill(Color.RED);
-                break;
-            default:
-                context.setFill(Color.BLUE);
-                break;
-        }
-        context.fillRect(100, 100, 250, 250);
+        System.out.println(keyEvent.getCode());
     }
 }
