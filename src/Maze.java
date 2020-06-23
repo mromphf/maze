126185b35b0f4b0f5e3f36590270d8414843fece
Mark Romphf <markromphf@gmail.com>
2020-06-22 19:30:36 -0600

    Draw a full box.

--- src/Maze.java
+++ src/Maze.java
@@ -7,7 +7,15 @@ public class Maze {
         for (int i = 0; i < 1500; i += 50) {
             drawables.add(new Block(i, 0));
         }
-        drawables.add(new Block(0, 50));
+        for (int i = 0; i < 1050; i += 50) {
+            drawables.add(new Block(0, i));
+        }
+        for (int i = 0; i < 1050; i += 50) {
+            drawables.add(new Block(1500, i));
+        }
+        for (int i = 0; i < 1500; i += 50) {
+            drawables.add(new Block(i, 1050));
+        }
         return drawables;
     }
 }
