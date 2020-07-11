a4fe0106123ed697703062d2a905f91fcefa5cb6
Mark Romphf <markromphf@gmail.com>
2020-07-11 14:15:55 -0600

    Don't need the Maze class. Delete that shiz.

--- src/game/GameLoop.java
+++ src/game/GameLoop.java
@@ -1,6 +1,8 @@
 package game;
 
 import game.abstraction.Movable;
+import game.abstraction.Tile;
+import game.concrete.Goal;
 import game.concrete.Player;
 import io.File;
 import javafx.animation.AnimationTimer;
@@ -12,6 +14,7 @@ import javafx.stage.Screen;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static javafx.application.Platform.exit;
 
@@ -22,8 +25,9 @@ public class GameLoop extends AnimationTimer {
     private final double screenWidth;
     private final double screenHeight;
     private final Map<Integer, List<Character>> levelFile = File.loadLevel("src/level1.csv");
-    private final Maze maze = new Maze(LoadsLevels.generateTiles(levelFile));
+    private final List<Tile> tiles = LoadsLevels.generateTiles(levelFile).stream().flatMap(List::stream).collect(Collectors.toList());
     private final Player player;
+    private final Goal goal;
 
     public GameLoop(Canvas fgCanvas, Canvas bgCanvas) {
         Rectangle2D screen = Screen.getPrimary().getBounds();
@@ -44,11 +48,17 @@ public class GameLoop extends AnimationTimer {
         foreground.translate(225, 35);
         background.translate(225, 35);
 
-        maze.tiles().forEach(d -> d.draw(background));
+        tiles.forEach(d -> d.draw(background));
 
         Collection<? extends Movable> movables = LoadsLevels.generateMovables(levelFile);
+
         player = (Player) movables.stream()
                 .filter(m -> m instanceof Player)
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+
+        goal = (Goal) tiles.stream()
+                .filter(Tile::isGoal)
                 .findFirst()
                 .orElseThrow(IllegalStateException::new);
     }
@@ -57,9 +67,9 @@ public class GameLoop extends AnimationTimer {
     public void handle(long now) {
         foreground.clearRect(0, 0, screenWidth, screenHeight);
         player.draw(foreground);
-        player.move(maze.tiles());
+        player.move(tiles);
 
-        if (maze.gameIsOver(player)) {
+        if (player.collidesWith(goal)) {
             gameOver();
         }
     }
