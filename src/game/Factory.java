d1b0674f5722fbbc9d4210529cd336ccc5653bd3
Mark Romphf <markromphf@gmail.com>
2020-07-11 19:09:11 -0600

    A constant for standard tile size might come in handy.

--- src/game/Factory.java
+++ src/game/Factory.java
@@ -8,26 +8,27 @@ import java.util.Optional;
 
 public class Factory {
 
+    private static final int TILE_SIZE = 50;
     private static final int TILE_OFFSET = 7;
 
     public static Tile buildTile(Character symbol, int vertical, int horizontal) {
         switch (symbol) {
             case 'b':
-                return new Block(vertical * 50, horizontal * 50);
+                return new Block(vertical * TILE_SIZE, horizontal * TILE_SIZE);
             case 's':
-                return new Start(vertical * 50, horizontal * 50);
+                return new Start(vertical * TILE_SIZE, horizontal * TILE_SIZE);
             case 'g':
-                return new Goal(vertical * 50, horizontal * 50);
+                return new Goal(vertical * TILE_SIZE, horizontal * TILE_SIZE);
             default:
-                return new Empty(vertical * 50, horizontal * 50);
+                return new Empty(vertical * TILE_SIZE, horizontal * TILE_SIZE);
         }
     }
 
     public static Optional<Actor> buildActor(Character symbol, int x, int y) {
         if (symbol == 's') {
             return Optional.of(new Player(
-                    (x * 50) + TILE_OFFSET,
-                    (y * 50) + TILE_OFFSET,
+                    (x * TILE_SIZE) + TILE_OFFSET,
+                    (y * TILE_SIZE) + TILE_OFFSET,
                     5));
         }
         return Optional.empty();
