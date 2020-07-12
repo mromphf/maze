acacd69b0db4f4634e85884849ea661969cb41ec
Mark Romphf <markromphf@gmail.com>
2020-07-12 13:45:14 -0600

    Don't need this method on the GameObject interface.

--- src/game/abstraction/GameObject.java
+++ src/game/abstraction/GameObject.java
@@ -2,17 +2,15 @@ package game.abstraction;
 
 import javafx.scene.canvas.GraphicsContext;
 
-import java.util.Collection;
-
 public interface GameObject {
 
     void draw(GraphicsContext context);
 
     boolean matches(Predicate p);
 
-    boolean collidesWith(GameObject target);
+    void onCollide(GameObject target);
 
-    boolean collidesWith(Collection<? extends GameObject> targets);
+    boolean collidesWith(GameObject target);
 
     int getX();
 
