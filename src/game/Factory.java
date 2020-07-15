ccfeeb8595154aa5c2c27497db3b315aacea8a60
Mark Romphf <markromphf@gmail.com>
2020-07-15 17:18:51 -0600

    Rename variables in the Factory class.

--- src/game/Factory.java
+++ src/game/Factory.java
@@ -11,31 +11,31 @@ public class Factory {
     private static final int TILE_SIZE = 55;
     private static final int TILE_OFFSET = 10;
 
-    public static Entity buildStaticTile(Character symbol, int vertical, int horizontal) {
+    public static Entity buildStaticTile(Character symbol, int x, int y) {
         switch (symbol) {
             case 'b':
-                return new Block(vertical * TILE_SIZE, horizontal * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                return new Block(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
             case 's':
-                return new Start(vertical * TILE_SIZE, horizontal * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                return new Start(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
             case 'p':
-                return new Pit(vertical * TILE_SIZE, horizontal * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                return new Pit(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
             default:
-                return new Empty(vertical * TILE_SIZE, horizontal * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                return new Empty(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
         }
     }
 
-    public static Optional<Entity> buildDynamicTile(Character symbol, int vertical, int horizontal) {
+    public static Optional<Entity> buildDynamicTile(Character symbol, int x, int y) {
         switch (symbol) {
             case 'g':
-                return Optional.of(new Goal(vertical * TILE_SIZE, horizontal * TILE_SIZE, TILE_SIZE, TILE_SIZE, false));
+                return Optional.of(new Goal(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, false));
             case 'x':
-                return Optional.of(new Switch(vertical * TILE_SIZE, horizontal * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+                return Optional.of(new Switch(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE));
             case '1':
             case '2':
             case '3':
             case '4':
                 int ordinal = symbol - '0';
-                return Optional.of(new OrderedSwitch(vertical * TILE_SIZE, horizontal * TILE_SIZE, TILE_SIZE, TILE_SIZE, ordinal));
+                return Optional.of(new OrderedSwitch(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, ordinal));
             default:
                 return Optional.empty();
         }
